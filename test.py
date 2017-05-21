d106b273fa670d2707ec3f1520c4a026b2625b15
Python3pkg <raliclo@gmail.com>
2017-05-21 13:52:53 -0700

    Convert to Python3

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import PyExceptionInfo
 
 def makeException():
-    print "Trying to create an exception"
+    print("Trying to create an exception")
     try:
         # The first line causes NameError, second line ZeroDivision
         happy()
-        print (8 / 0)
+        print((8 / 0))
     except Exception as e:
         PyExceptionInfo.getInfo(e)
 
